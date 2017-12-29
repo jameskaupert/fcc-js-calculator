e15cdeeb148a984de7605404b669f058048a358e
James Kaupert <jameskaupert@gmail.com>
2017-12-29 07:00:17 -0500

    initial app changes from skeleton

---
+++ src/components/CalculatorApp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class CalculatorApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Calculator</h1>
+      </div>
+    );
+  }
+}

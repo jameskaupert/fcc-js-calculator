96a4c35b8aa34b683f3e113f6372e49e50caf439
James Kaupert <jameskaupert@gmail.com>
2017-12-29 14:28:34 -0500

    get most of the functionality working

--- src/components/CalculatorApp.js
+++ src/components/CalculatorApp.js
@@ -4,18 +4,117 @@ import Keypad from "./Keypad";
 
 export default class CalculatorApp extends React.Component {
   state = {
-    currentCalculation: '4x2=8',
-    currentOutput: 0
-  }
+    currentCalculation: "",
+    currentOutput: "0"
+  };
+  handleClick = e => {
+    let key = e.target.innerHTML;
+    if (key === 'x') {
+      key = '*'
+    }
+    this.updateCurrentOutput(key);
+    this.updateCalculation(key);
+  };
+  updateCurrentOutput = key => {
+    const operators = ["+", "-", "*", "/", "="];
+    let currentOutput;
+    if (key === "AC") {
+      currentOutput = "0";
+    } else if (key === "=") {
+      currentOutput = this.evaluateCalculation();
+    } else if (this.state.currentOutput === "0") {
+      if (key === ".") {
+        currentOutput = "0.";
+      } else {
+        currentOutput = key;
+      }
+    } else if (!operators.includes(this.state.currentOutput)) {
+      if (operators.includes(key)) {
+        currentOutput = key;
+      } else if (key === ".") {
+        if (this.state.currentOutput.includes(".")) {
+          currentOutput = this.state.currentOutput;
+        } else {
+          currentOutput = this.state.currentOutput + key;
+        }
+      } else {
+        currentOutput = this.state.currentOutput + key;
+      }
+    } else {
+      if (key === ".") {
+        currentOutput = "0" + key;
+      } else {
+        currentOutput = key;
+      }
+    }
+    this.setState(() => ({ currentOutput }));
+  };
+  updateCalculation = key => {
+    const operators = ["+", "-", "*", "/", "="];
+    let currentCalculation;
+    if (key === "AC") {
+      currentCalculation = "";
+    } else if (key === '=') {
+      currentCalculation = this.state.currentCalculation + '=' + this.evaluateCalculation()
+    }
+    else if (this.state.currentCalculation === "") {
+      if (operators.includes(key) || key === ".") {
+        currentCalculation = "0" + key;
+      } else {
+        currentCalculation = key;
+      }
+    } else if (
+      operators.includes(
+        this.state.currentCalculation[this.state.currentCalculation.length - 1]
+      )
+    ) {
+      if (operators.includes(key)) {
+        currentCalculation = this.state.currentCalculation.slice(0, -1) + key;
+      } else {
+        if (key === ".") {
+          currentCalculation = this.state.currentCalculation + "0" + key;
+        } else {
+          currentCalculation = this.state.currentCalculation + key;
+        }
+      }
+    } else if (
+      this.state.currentCalculation[
+        this.state.currentCalculation.length - 1
+      ] === "0"
+    ) {
+      if (key === 0) {
+        currentCalculation = this.state.currentCalculation;
+      } else if (operators.includes(key) || key === ".") {
+        currentCalculation = this.state.currentCalculation + key;
+      } else {
+        currentCalculation = this.state.currentCalculation.slice(0, -1) + key;
+      }
+    } else {
+      if (key === ".") {
+        const operands = this.state.currentCalculation.split(/[\+\-\/\*]/g);
+        if (operands[operands.length - 1].indexOf(".") === -1) {
+          currentCalculation = this.state.currentCalculation + key;
+        } else {
+          currentCalculation = this.state.currentCalculation;
+        }
+      } else {
+        currentCalculation = this.state.currentCalculation + key;
+      }
+    }
+    this.setState(() => ({ currentCalculation }));
+  };
+  evaluateCalculation = () => {
+    return eval(this.state.currentCalculation);
+  };
   render() {
     return (
       <div id="calculator-app" className="calculator-app">
         <div className="calculator-wrapper">
-        <Display
-          currentCalculation={this.state.currentCalculation}
-          currentOutput={this.state.currentOutput}
-        />
-        <Keypad />
+          <Display
+            currentCalculation={this.state.currentCalculation}
+            currentOutput={this.state.currentOutput}
+          />
+          <Keypad handleClick={this.handleClick} />
         </div>
       </div>
     );
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -3,65 +3,81 @@ import React from "react";
 const Keypad = props => {
   return (
     <div id="keypad" className="keypad">
-      <button id="clear" className="clear-btn">
+      <button id="clear" className="clear-btn" onClick={props.handleClick}>
         AC
       </button>
-      <button id="divide" className="divide-btn">
+      <button id="divide" className="divide-btn" onClick={props.handleClick}>
         /
       </button>
-      <button id="multiply" className="multiply-btn">
+      <button
+        id="multiply"
+        className="multiply-btn"
+        onClick={props.handleClick}
+      >
         x
       </button>
 
-      <button id="seven" className="seven-btn">
+      <button id="seven" className="seven-btn" onClick={props.handleClick}>
         7
       </button>
-      <button id="eight" className="eight-btn">
+      <button id="eight" className="eight-btn" onClick={props.handleClick}>
         8
       </button>
-      <button id="nine" className="nine-btn">
+      <button id="nine" className="nine-btn" onClick={props.handleClick}>
         9
       </button>
-      <button id="subtract" className="subtract-btn">
+      <button
+        id="subtract"
+        className="subtract-btn"
+        onClick={props.handleClick}
+      >
         -
       </button>
 
-      <button id="four" className="four-btn">
+      <button id="four" className="four-btn" onClick={props.handleClick}>
         4
       </button>
-      <button id="five" className="five-btn">
+      <button id="five" className="five-btn" onClick={props.handleClick}>
         5
       </button>
-      <button id="six" className="six-btn">
+      <button id="six" className="six-btn" onClick={props.handleClick}>
         6
       </button>
-      <button id="add" className="add-btn">
+      <button id="add" className="add-btn" onClick={props.handleClick}>
         +
       </button>
 
       <div className="rows-3-4">
         <div className="cols-0-2">
           <div className="row-3">
-            <button id="one" className="one-btn">
+            <button id="one" className="one-btn" onClick={props.handleClick}>
               1
             </button>
-            <button id="two" className="two-btn">
+            <button id="two" className="two-btn" onClick={props.handleClick}>
               2
             </button>
-            <button id="three" className="three-btn">
+            <button
+              id="three"
+              className="three-btn"
+              onClick={props.handleClick}
+            >
               3
             </button>
           </div>
           <div className="row-4">
-            <button id="zero" className="zero-btn">
+            <button id="zero" className="zero-btn" onClick={props.handleClick}>
               0
             </button>
-            <button id="decimal" className="decimal-btn">
+            <button
+              id="decimal"
+              className="decimal-btn"
+              onClick={props.handleClick}
+            >
               .
             </button>
           </div>
         </div>
-        <button id="equals" className="equals-btn">
+        <button id="equals" className="equals-btn" onClick={props.handleClick}>
           =
         </button>
       </div>
